bd0cde55d597bd7118bb83bc2f01d3c03b56e8e0
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-10 23:52:25 +0400

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h2>Page not found</h2>
+    </div>
+  );
+};
+
+export default NotFound;
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 const ReviewPage = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       Review product {params.productId} review {params.reviewId}
