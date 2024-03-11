4c6fe8e3f6ffd8ec460773d5faecc6b197ea8012
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-12 00:35:50 +0400

    routing Metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "About Codevolution",
+  description: "About page desc",
+};
+
 const About = () => {
   return <div>about page</div>;
 };
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { Metadata } from "next";
 
-const ProductDetail = ({ params }: { params: { productId: string } }) => {
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({ params }: Props): Metadata => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+const ProductDetail = ({ params }: Props) => {
   return <div>Details about product {params.productId}</div>;
 };
 
