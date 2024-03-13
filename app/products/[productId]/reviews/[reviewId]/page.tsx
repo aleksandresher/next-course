65edbc43a3a81a50537c855d0aa0ab6d4d5bc30f
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-13 12:48:25 +0400

    error handling

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { notFound } from "next/navigation";
+import { count } from "console";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 
 const ReviewPage = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
