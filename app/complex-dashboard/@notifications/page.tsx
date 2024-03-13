210c734bd76cfceeb46748ef797e24e3dfd1cec8
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-13 22:11:54 +0400

    unmatched routes

--- app/complex-dashboard/@notifications/page.tsx
+++ app/complex-dashboard/@notifications/page.tsx
@@ -1,4 +1,10 @@
 import Card from "@/components/card";
+import Link from "next/link";
 export default function Notifications() {
-  return <Card>Notification</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
